6465177fd8b56d29d0ad02c78af2a7e5de8b1340
aysunitai <aysun.itai@gmail.com>
2022-12-15 03:49:22 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo.js'
+import Download from './components/Download/Download';
+import Signin from './components/Signin/Signin.js';
+import Register from './components/Register/Register.js';
 import './App.css';
 
-function App() {
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      route:'signin',
+      isSignedIn:false
+    }
+  }
+  
+  onRouteChange=(route)=>{
+    if(route==='signout'){
+      this.setState({isSignedIn:false})
+    }else if(route==='download'){
+      this.setState({isSignedIn:true})
+    
+  }
+    this.setState({route:route});
+  }
+  render(){
+    const {isSignedIn,route } = this.state
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+     <Logo/>
+     {route==='download' ? <Download/>
+     :(
+      route==='signin' ?
+      <Signin onRouteChange={this.onRouteChange}/> 
+      :
+      <Register onRouteChange={this.onRouteChange}/>
+
+     )
+       }
+     
     </div>
   );
+  }
 }
 
 export default App;
