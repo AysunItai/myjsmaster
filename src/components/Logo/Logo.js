cd553387d53d1f4a7208a2107c0eb236ffba6a37
aysunitai <aysun.itai@gmail.com>
2022-12-17 10:35:56 +0200

    'logo1'

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import myjsmasterlogo from './myjsmaster.svg';
 
 const Logo = () =>{
     return (
         <div className='ma4 mt0'>
+            <img alt='logo' src={myjsmasterlogo}/>
 
         </div>
         
