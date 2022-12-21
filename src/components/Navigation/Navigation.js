d00da16eb3c6ee5bfdfb62f1253ddc58b819ab87
aysunitai <aysun.itai@gmail.com>
2022-12-21 08:22:09 +0200

    download4

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,10 +7,11 @@ const Navigation = ({onRouteChange,isSignedIn}) =>{
         
         if(isSignedIn){
             return(
-           <nav style={{display:'flex',justifyContent:'flex-end'}}>
+           <nav style={{display:'flex ', alignItems:'center',justifyContent:'space-around',color:'white',background:`url(${bg}) center`,padding:'5px'}}>
             <Logo onRouteChange={onRouteChange}/>
-    
+             <div style={{display:'flex ', alignItems:'center'}}>
             <p onClick={()=>onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+           </div>
            </nav>
             )
 
